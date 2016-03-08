5722e89291779f9747bbfa4cf2030447e0abb4a9
Scott Woodall <scott.woodall@gmail.com>
2016-03-08 10:54:45 -0500

    Bugfix: exception was being thrown when creating superusers

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,14 +1,61 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
-                                        UserManager)
+                                        BaseUserManager)
 from django.db import models
 from django.utils import timezone
+
+
+class EmailUserManager(BaseUserManager):
+    def _create_user(self, email, password, is_staff, is_superuser,
+                     **extra_fields):
+        now = timezone.now()
+
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        is_active = extra_fields.pop("is_active", True)
+
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=is_active,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        is_staff = extra_fields.pop("is_staff", False)
+
+        return self._create_user(
+            email,
+            password,
+            is_staff,
+            False,
+            **extra_fields
+        )
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(
+            email, password,
+            True,
+            True,
+            **extra_fields
+        )
 
 
 class EmailUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
     email = models.EmailField(
         max_length=254,
         unique=True,
@@ -16,10 +63,11 @@ class EmailUser(AbstractBaseUser, PermissionsMixin):
             'unique': 'That email address is already taken.'
         }
     )
+
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    objects = UserManager()
+    objects = EmailUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
